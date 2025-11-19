9f7605580e13a670d68ebbdfe1522dfd7965bb81
Skylar Pape <skylar.p@movementcooperative.org>
2025-11-18 19:51:11 -0600

    Changes venv based on directory

--- config.fish
+++ config.fish
@@ -7,6 +7,21 @@ end
 
 # TERMINAL PROMPT
 function fish_prompt -d "Write out the prompt" 
+	# PYTHON VIRTUAL ENVIRONMENT
+	set PYTHON_VIRTUAL_ENVIRONMENT ""
+	# Toggle virtual environment
+	if test -L .venv && test -d .venv
+		source .venv/bin/activate.fish
+	# If environment from previous directory is currently activated
+	else if test -n "$VIRTUAL_ENV"
+		deactivate
+	end
+	# Get environment name, if one is active
+	if test -n "$VIRTUAL_ENV"
+		set PYTHON_VIRTUAL_ENVIRONMENT "$(basename $VIRTUAL_ENV) "	
+	end	
+	
+	# GCLOUD PROJECT
 	if not string length --quiet "$(which gcloud)"
 		init_gcloud
 	end	
@@ -14,13 +29,16 @@ function fish_prompt -d "Write out the prompt"
 	if test "$GCLOUD_PROJECT" = "(unset) "
 		set GCLOUD_PROJECT ""
 	end
+
+	# GIT BRANCH
 	set GIT_BRANCH ""
 	if git branch --show-current &> /dev/null	
 		set GIT_BRANCH "$(git branch --show-current) "
 	end
 	set CURRENT_DIRECTORY_PATH "$(prompt_pwd)
 "
-	printf '%s%s%s%s%s' \
+	printf '%s%s%s%s%s%s' \
+		(set_color blue) "$PYTHON_VIRTUAL_ENVIRONMENT" (set_color normal) \
 		(set_color cyan) "$GCLOUD_PROJECT" (set_color normal) \
 		(set_color magenta) "$GIT_BRANCH" (set_color normal) \
 		(set_color $fish_color_cwd) "$CURRENT_DIRECTORY_PATH" (set_color normal)
@@ -37,7 +55,7 @@ fish_add_path "$HOME/Local/mxskylar/scripts"
 fish_add_path "$HOME/Local/mxskylar/python-scripts/bin"
 
 # CONVENIENCE VARIABLES
-set -x FISH_CONFIG "$HOME/.config/fish/config.fish"
+set -gx FISH_CONFIG "$HOME/.config/fish/config.fish"
 
 # HOMEBREW
 eval (/opt/homebrew/bin/brew shellenv)
@@ -49,13 +67,17 @@ if test -d (brew --prefix)"/share/fish/vendor_completions.d"
 end
 
 # PYTHON
-set -x PYTHONPATH "$HOME/scripts" "$PYTHONPATH"
+set -gx PYTHONPATH "$HOME/scripts" "$PYTHONPATH"
 fish_add_path $(brew --prefix)"/opt/python@3.11/libexec/bin"
+set -gx VIRTUAL_ENV_DISABLE_PROMPT 1 # The prompt is stale when changing directories while the active virtual environment is not
 
 # DBT
-source $HOME/.venv/dbt/bin/activate.fish
-set -x DBT_ENV_SECRET_GIT_CREDENTIAL "$(op item get "DBT Dev Environment Git Credential" --vault "Employee" --format json --fields password | jq .value --raw-output)"
-set -x DEV_SCHEMA_NAME "skylarpape"
+set -gx DBT_ENV_SECRET_GIT_CREDENTIAL "$(op item get "DBT Dev Environment Git Credential" --vault "Employee" --format json --fields password | jq .value --raw-output)"
+set -gx DEV_SCHEMA_NAME "skylarpape"
+
+# JAVA
+set -gx JAVA_HOME "$(brew --prefix)/opt/openjdk@17"
+fish_add_path "$JAVA_HOME/bin" 
 
 # ABBREVIATIONS
 # os utils
